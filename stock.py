6f1f15cdb3b7255b770ffbe01544528318571d04
MiguelSzntos <245016598+MiguelSzntos@users.noreply.github.com>
2026-02-20 14:21:23 -0300

    Adição de Stock

--- stock.py
+++ stock.py
@@ -5,3 +5,19 @@ def estoque(page: ft.Page):
     page.bgcolor = ft.Colors.BLACK_45
 
     
+header = ft.Row(
+        controls=[
+            ft.Text("Consultar Estoque", size=24, weight="bold", color="white"),
+            ft.Container(
+                content=ft.Image(
+                    src="imgs/addicon.png",
+                    width=35,
+                    height=35,
+                    fit=ft.ImageFit.CONTAIN,
+                ),
+                on_click=lambda _: print("Botão adicionar clicado!"),
+                mouse_cursor=ft.MouseCursor.CLICK,
+            )
+        ],
+        alignment="spaceBetween"
+    )
