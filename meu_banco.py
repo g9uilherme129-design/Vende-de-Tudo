c44d015f0b9c97edc5a7fc5ec177cfa2e7ca5970
scooby47-boop <tatubola360wmgs@gmail.com>
2026-03-09 15:20:32 -0300

    Update meu_banco.py
    criação da rota do jogo

--- meu_banco.py
+++ meu_banco.py
@@ -11,7 +11,8 @@ def criar_tabelas():
             email TEXT UNIQUE NOT NULL,
             senha_user TEXT,
             status TEXT NOT NULL,
-            perfil TEXT NOT NUL
+            perfil TEXT NOT NUL,
+            venda TEXT NOT NULL
         )
     ''')
 
@@ -32,36 +33,43 @@ def criar_tabelas():
             id_categoria INTEGER NOT NULL,
             nome_produto TEXT NOT NULL,
             codigo_barra TEXT NOT NULL,
-            id_fornecedor INTEGER,
-            id_categoria INTEGER,
-            FOREIGN KEY (id_fornecedor) REFERENCCES fornecedores (id),
-            FOREIGN KEY (id_categoria) REFERENCCES categorias (id)                       
+            FOREIGN KEY (id_fornecedor) REFERENCCES fornecedores (id_fornecedor),
+            FOREIGN KEY (id_categoria) REFERENCCES categorias (id_categoria)                  
         )
     ''')
 
 
-def venda():
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS fornecedor(
+            id_fornecedor INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome_fornecedor TEXT NOT NULL,
+            cnpj INTEGER NOT NULL,
+            contato INTEGER NOT NULL,
+            email TEXT NOT NULL, 
+            rua TEXT NOT NULL,
+            cep INTEGER NOT NULL
+    ''')
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS vendas(
-            id_vendas INTEGER PRIMARY KEY AUTOINCREMENT,
-            data_venda DATETIME DEFAULT CURRENT_TIMESTAMP,
-            total REAL NOT NULL,
-            metodo_pagamento TEXT
-            
-            
+            id_itens INTEGER NOT NULL,
+            id_user INTEGER NOT NULL,
+            FOREIGN KEY (id_item ) REFERENCCES itens_venda (id_item),
+            FOREIGN KEY (id_user) REFERENCCES categorias (id_user) 
         )
     ''')
 
-def itens_venda():
-        cursor.execute('''
-        CREATE TABLE IF NOT EXISTS vendas(
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS itens_vendidos(
             id_item INTEGER PRIMARY KEY AUTOINCREMENT,
-            id_venda INTEGER NOT NULL,
+            id_user INTEGER NOT NULL,
             id_produto INTEGER NOT NULL,
             quantidade INTEGER NOT NULL,
             preco_unitario REAL NOT NULL,
-            FOREIGN KEY (id_venda) REFERENCES vendas (id_venda),
-            FOREIGN KEY (id_produto) REFERENCES produto (id_venda)
+            metodo_pagamento TEXT,
+            data_venda DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (id_user) REFERENCES vendas (id_user),
+            FOREIGN KEY (id_produto) REFERENCES produto (id_produto)
             
             
         )
